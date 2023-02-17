908ac375a013ed6c38b323024983268216a3917c
Thiyaga Rajan <3195918+trajan5659@users.noreply.github.com>
2023-02-17 21:03:57 +0800

    ver3

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-import numpy as np
-import time
-import sys
+import pandas as pd
 
-S = range(1000)
+# Read the CSV file into a DataFrame object
+df = pd.read_csv('C:/Users/USER/Desktop/filename.csv')
 
+# Print the first five rows of the DataFrame
+print(df.head())
 
-print(sys.getsizeof(5)*len(S))
+# Filter the DataFrame to include only rows where the 'column_name' column is greater than 5
+filtered_df = df[df['column_name'] > 2]
 
-D = np.arange(1000)
+# Print the filtered DataFrame
+#print(filtered_df)
 
-print(D.size*D.itemsize)
+# Compute the mean of the 'column_name' column
+#mean_value = df['column_name'].mean()
+
+# Print the mean value
+#print(mean_value)
+
+# Save the filtered DataFrame to a new CSV file
+#filtered_df.to_csv('C:/Users/USER/Desktop/new_filename.csv')
