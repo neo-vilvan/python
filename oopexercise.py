4f887806ec2877c35757ad69329778871286dd9a
Thiyaga Rajan <3195918+trajan5659@users.noreply.github.com>
2023-02-18 11:33:14 +0800

    opp_traning

--- oopexercise.py
+++ oopexercise.py
@@ -1,114 +1,36 @@
-import tkinter as tk
+class Item:
+    pay_rate = 0.8 # pay rate after 20% discount
+    all = []
+    def __init__(self, name: str, price: float, quantity=0):
+        #Run validation to the received arguments
+        assert price >= 0, f" Price {price} not greater than zero"
+        assert quantity >= 0, f" Quantity {quantity} not greater than zero"
 
-class Bank:
-    def __init__(self):
-        self.customers = []
-
-    def create_customer(self, name, age, address):
-        customer = Customer(name, age, address)
-        self.customers.append(customer)
-
-    def create_account(self, index):
-        self.customers[index].create_account()
-
-    def deposit(self, index, amount):
-        self.customers[index].deposit(amount)
-
-    def withdraw(self, index, amount):
-        self.customers[index].withdraw(amount)
-
-    def get_balance(self, index):
-        return self.customers[index].get_balance()
-
-class Customer:
-    def __init__(self, name, age, address):
+        
+        #Assign to self object
         self.name = name
-        self.age = age
-        self.address = address
-        self.accounts = [Account()]
+        self.price = price
+        self.quantity = quantity
+        
+        #Actions to execute
+        Item.all.append(self)
 
-    def create_account(self):
-        account = Account()
-        self.accounts.append(account)
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
-    def deposit(self, amount):
-        self.accounts[0].deposit(amount)
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
-    def withdraw(self, amount):
-        self.accounts[0].withdraw(amount)
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-    def get_balance(self):
-        return self.accounts[0].balance
+item1 = Item("Phone", 100, 1 )
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-class Account:
-    def __init__(self):
-        self.balance = 0
+print(Item.all)
 
-    def deposit(self, amount):
-        self.balance += amount
 
-    def withdraw(self, amount):
-        if amount > self.balance:
-            print("Insufficient funds")
-        else:
-            self.balance -= amount
 
-class BankGUI:
-    def __init__(self, master):
-        self.bank = Bank()
-
-        self.master = master
-        master.title("Bank Application")
-
-        self.create_customer_button = tk.Button(master, text="Create Customer", command=self.create_customer)
-        self.create_customer_button.pack()
-
-        self.customer_index_label = tk.Label(master, text="Customer Index:")
-        self.customer_index_label.pack()
-        self.customer_index_entry = tk.Entry(master)
-        self.customer_index_entry.pack()
-
-        self.create_account_button = tk.Button(master, text="Create Account", command=self.create_account)
-        self.create_account_button.pack()
-
-        self.deposit_label = tk.Label(master, text="Deposit Amount:")
-        self.deposit_label.pack()
-        self.deposit_entry = tk.Entry(master)
-        self.deposit_entry.pack()
-
-        self.deposit_button = tk.Button(master, text="Deposit", command=self.deposit)
-        self.deposit_button.pack()
-
-        self.withdraw_label = tk.Label(master, text="Withdrawal Amount:")
-        self.withdraw_label.pack()
-        self.withdraw_entry = tk.Entry(master)
-        self.withdraw_entry.pack()
-
-        self.withdraw_button = tk.Button(master, text="Withdraw", command=self.withdraw)
-        self.withdraw_button.pack()
-
-        self.balance_button = tk.Button(master, text="Check Balance", command=self.check_balance)
-        self.balance_button.pack()
-
-        self.quit_button = tk.Button(master, text="Quit", command=master.quit)
-        self.quit_button.pack()
-
-    def create_customer(self):
-        self.customer_window = tk.Toplevel(self.master)
-
-        self.name_label = tk.Label(self.customer_window, text="Name:")
-        self.name_label.pack()
-        self.name_entry = tk.Entry(self.customer_window)
-        self.name_entry.pack()
-
-        self.age_label = tk.Label(self.customer_window, text="Age:")
-        self.age_label.pack()
-        self.age_entry = tk.Entry(self.customer_window)
-        self.age_entry.pack()
-
-        self.address_label = tk.Label(self.customer_window, text="Address:")
-        self.address_label.pack()
-        self.address_entry = tk.Entry(self.customer_window)
-        self.address_entry.pack()
-
-        self.submit_button = tk.Button(self.customer_window
