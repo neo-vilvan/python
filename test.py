7dd999730bac7a680061815c1c9131d672891a68
Thiyaga Rajan <3195918+trajan5659@users.noreply.github.com>
2023-03-17 13:14:40 +0800

    train

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
+def binary_search(sequence, item):
+    begin_index = 0
+    end_index = len(sequence) - 1
 
-class BankCustomer:
-    def __init__(self, accNo, name, ic, job, accountType):
-        self.accNo = accNo
-        self.name = name
-        self.ic = ic
-        self.job = job
-        self.accountType = accountType
+    while begin_index <= end_index:
+        midpoint = begin_index + (end_index - begin_index) // 2
+        midpoint_value = sequence[midpoint]
+        if midpoint_value == item:
+            return midpoint
+        
+        elif item < midpoint_value:
+            end_index = midpoint - 1
 
-bankCust1 = BankCustomer(451254, "Raj", "123456-12-1234", "Teacher", "Saving")
-print(bankCust1. __dict__)
+        else:
+            begin_index = midpoint + 1
+
+    return None
+
+sequence_a = [2,4,5,6,7,8,9,10,11,25]
+item_a = 5
+
+print(binary_search(sequence_a, item_a))
+
+k = 9 // 2
+print(k)
---
+++ tkintertraining.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+
+label1 = Label(root, text="Label 1")
+label1.pack()
+
+label2 = Label(root, text="Label 2")
+label2.pack()
+
+label3 = Label(root, text="Label 3")
+label3.pack()
+
+root.mainloop()
